1278350124f152a400f1bd1ef28899ad02196840
Kristan Johns <kristandjohns@gmail.com>
2019-09-09 21:51:19 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,19 +1,1 @@
 # Use this space to code along with the readme
-
-phrase = "Hello World!"
-puts phrase
-Hello World
-nil
-puts phrase
-Hello World
-nil
-puts phrase
-def say_hello_world_ten_times
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-end
