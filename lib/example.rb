6b6c35ccccfc917ded47ba0490420b3f167518e8
Kristan Johns <kristandjohns@gmail.com>
2019-09-09 21:44:35 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -2,12 +2,17 @@
 
 phrase = "Hello World!"
 puts phrase
+Hello World
+puts phrase
+Hello World
+puts phrase
+Hello World
+puts phrase
+def say_hello_ten_times
 puts phrase
 puts phrase
 puts phrase
 puts phrase
 puts phrase
 puts phrase
-puts phrase
-puts phrase
-puts phrase
+end
