59a6ab8c19efe1a8f36a9aa6fab43a21e4d4a15a
Kristan Johns <kristandjohns@gmail.com>
2019-09-09 21:53:43 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -8,4 +8,12 @@ nil
 def say_hello_ten_times
   phrase = "Hello world"
   puts phrase
-  
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase 
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
